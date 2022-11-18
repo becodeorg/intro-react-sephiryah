918b694325b5bc0b85650486e1ebf026aadd92ba
Sephirah <nicolas.bussiere@outlook.fr>
2022-11-18 14:33:08 +0100

    Added state for list and checkbox

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,18 +1,33 @@
+import React, { useState } from 'react';
+
 function Checkbox() {
+    const [check, setCheck] = useState(false);
+
+    const handleChange = event => {
+        if (event.target.checked) {
+          console.log('✅ Checkbox is checked');
+        } else {
+          console.log('⛔️ Checkbox is NOT checked');
+        }
+        setCheck(current => !current);
+      };
+
     return (
-        <input type='checkbox'></input>
+        <input type='checkbox' value={check} onChange={handleChange}></input>
     );
 }
 
 export default function TodoList() {
+    const initialTodos = ["Sleep (a lot)", "Download TailwindCSS", "Finish Todo List application"];
+    const [todos, setTodos] = useState(initialTodos);
     return (
         <div>
             <h2 className="text-2xl font-bold mb-5">My Todo-list</h2>
             <div className="mx-72">
                 <ul>
-                    <li className="mb-3 text-left"><Checkbox /> Make a to-do-list</li>
-                    <li className="mb-3 text-left"><Checkbox /> Feed my cat</li>
-                    <li className="mb-3 text-left"><Checkbox /> Sleep (a lot)</li>
+                    {todos.map((todo) => (
+                    <li className="mb-3 text-left" key={todo}><Checkbox /> {todo}</li>
+                    ))}
                 </ul>
             </div>
         </div>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/index.html",
+    "./src/App.js",
+    "./src/components/Header.js",
+    "./src/components/New_todo.js",
+    "./src/components/Todolist.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
